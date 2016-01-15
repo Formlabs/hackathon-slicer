56869f918672bd7da5fce2874f6f89dc6610e0f9
Martin Galese <martin@galese.net>
2016-01-14 19:38:07 -0500

    Parse binary STL to object

--- app/js/loadModel.js
+++ app/js/loadModel.js
@@ -1,7 +1,11 @@
 'use strict';
+let parseSTL = require('parse-stl-binary');
 
 module.exports = loadModel;
 
 function loadModel(STLasArrayBuffer) {
+    let buf = new Buffer(STLasArrayBuffer);
+    let mesh = parseSTL(buf);
+    console.log(mesh);
     console.log(STLasArrayBuffer.byteLength);
 }
